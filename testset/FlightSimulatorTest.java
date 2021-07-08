86551bfc7891c10578f6046b5ae0292d7071e15e
jules-pierce <jupierce@seas.upenn.edu>
2021-07-07 23:24:30 -0600

    fix style

--- testset/FlightSimulatorTest.java
+++ testset/FlightSimulatorTest.java
@@ -1,18 +1,12 @@
 import static org.junit.Assert.*;
 
-import java.util.List;
-
-import org.junit.Before;
 import org.junit.Test;
-
 
 public class FlightSimulatorTest {
 
-	
-    @Test(expected=IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void testPlanesNull() {
-	FlightSimulator.simulateFlights(null, 0, 0);
+        FlightSimulator.simulateFlights(null, 0, 0);
     }
-
 
 }
